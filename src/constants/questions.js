60ca84e82e4105e137b0ccb701bdb880777ece9b
jovialp <32453962+jovialp@users.noreply.github.com>
2023-07-06 17:19:34 +0530

    1 binaries uploaded

---
+++ src/constants/questions.js
@@ -0,0 +1,53 @@
+const QUESTIONS = [
+  {
+    question:
+      'How do you judge what should be added in the next version of the app?',
+    options: [
+      'JavaScript is a scripting language used to make the website interactive',
+      'JavaScript is an assembly language used to make the website interactive',
+      'JavaScript is a compiled language used to make the website interactive',
+      'None of the mentioned',
+    ],
+  },
+  {
+    question:
+      'Which of the following is correct about JavaScript?',
+    options: [
+      'JavaScript is an Object-Based language',
+      'JavaScript is Assembly-language',
+      'JavaScript is an Object-Oriented language',
+      'JavaScript is a High-level language',
+    ],
+  },
+  {
+    question:
+      'Among the given statements, which statement defines closures in JavaScript?',
+    options: [
+      'JavaScript is a function that is enclosed with references to its inner function scope',
+      ' JavaScript is a function that is enclosed with references to its lexical environment',
+      'JavaScript is a function that is enclosed with the object to its inner function scope',
+      ' None of the mentioned',
+    ],
+  },
+  {
+    question:
+      'What will be the output of the following JavaScript code?',
+    options: [
+      '10',
+      '50',
+      '5',
+      'Error',
+    ],
+    image:
+  },
+  {
+    question:
+      'How do you judge what should be added in the next version of the app?',
+    options: [
+      'JavaScript is a scripting language used to make the website interactive',
+      'JavaScript is an assembly language used to make the website interactive',
+      'JavaScript is a compiled language used to make the website interactive',
+      'None of the mentioned',
+    ],
+  },
+];
--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import './home.css';
 
 // Assets
 import { logo } from '../../assets';
 
 const Home = () => {
+  const [questions, setQuestions] = useState();
+
+  const getQuestions = () => {
+
+
+  };
+
   return (
     <div className="home_screen_wrap">
+      {questions?.length && (
+          <Navigate to="/question/1" replace={true} />
+        )}
       <img src={logo} alt="logo" className="home_screen_logo" />
       <div className="home_screen_body">
         <h2 className="home_screen_title">Quiz</h2>
       </div>
-      <button className="home_screen_start">Start</button>
+      <button className="home_screen_start" onClick={getQuestions}>
+        Start
+      </button>
     </div>
   );
 };
